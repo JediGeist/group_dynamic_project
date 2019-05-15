07032751769b31df5a7314dc1a5d0b6dfd3ff1c3
staroverovad <staroverovad@yandex-team.ru>
2019-05-16 01:04:29 +0300

    Delete trash fies

--- model.py
+++ model.py
@@ -3,4 +3,11 @@ import pickle
 def get_model(path='./model.dat'):
     with open(path, 'rb') as f:
         model = pickle.load(f)
+#    print(type(model))
     return model
+
+#def main(): 
+#    get_model()
+#
+#if __name__ == '__main__':  #
+#    main()  #
